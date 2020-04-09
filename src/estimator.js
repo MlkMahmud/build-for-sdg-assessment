accd167b1a54036f4cff8b9bef832282632ac2f9
Malik Mahmud <almalikmahmud@gmail.com>
2020-04-09 18:16:01 +0100

    Complete challenge one

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,12 @@
-const covid19ImpactEstimator = (data) => data;
+import {
+  computeCurrentInfections,
+  computeInfectionsByTime
+} from './helpers';
+
+
+const covid19ImpactEstimator = (data) => {
+  const currentInfections = computeCurrentInfections(data);
+  return computeInfectionsByTime(currentInfections);
+};
 
 export default covid19ImpactEstimator;
